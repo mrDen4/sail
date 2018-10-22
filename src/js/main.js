480beda07eec6ef17d2ec0aad7ac5767b6b99957
Denis Davydov <den.saint.k@gmail.com>
2018-10-22 18:27:38 +0300

    add slider in hero section

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
   $('.header__menu').on('click', function() {
     $('.menu').toggleClass('menu--active');
   });
+
+  $('.hero__slider').slick({
+    prevArrow: $('.hero__arrow-l'),
+    nextArrow: $('.hero__arrow-r')
+  });
 });
