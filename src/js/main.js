70d4347763d8a31df477381296f5652339c24267
Denis Davydov <den.saint.k@gmail.com>
2018-10-25 10:27:26 +0300

    add popups

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,26 @@ $(document).ready(function () {
     $('.menu').toggleClass('menu--active');
   });
 
+  $('.content__btn, .item__btn').on('click', function() {
+    $('.popup').addClass('popup--active');
+    $('.bg').addClass('bg--active');
+  });
+
+  $('.popup__exit, .bg').on('click', function() {
+    $('.popup').removeClass('popup--active');
+    $('.bg').removeClass('bg--active');
+  });
+
+  $('.slide__btn').on('click', function() {
+    $('.popup-reserve').addClass('popup-reserve--active');
+    $('.bg').addClass('bg--active');
+  });
+
+  $('.popup__exit, .bg').on('click', function() {
+    $('.popup-reserve').removeClass('popup-reserve--active');
+    $('.bg').removeClass('bg--active');
+  });
+
   $('.hero__slider').slick({
     prevArrow: $('.hero__arrow-l'),
     nextArrow: $('.hero__arrow-r')
