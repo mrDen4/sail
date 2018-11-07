64656b38073b4977d28647321ba8ddd573bfaa95
Denis Davydov <den.saint.k@gmail.com>
2018-11-07 13:03:02 +0300

    add some changes

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+  $('.nav__item--summer').on('click', function() {
+    for (var i=0; i<16; i++) {
+      if ($('.all__item').dataset.season === "1") {
+        this.hide;
+        console.log('click');
+      }
+    }
+  });
+
   $('.header__menu, .header__location').on('click', function() {
     $('.menu').toggleClass('menu--active');
   });
