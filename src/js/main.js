abaa5d8a2bba8d042105d53c5cbaff86993a2d31
Denis Davydov <den.saint.k@gmail.com>
2018-11-07 16:39:50 +0300

    add some changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,60 @@
 $(document).ready(function () {
 
-  $('.nav__item--summer').on('click', function() {
-    for (var i=0; i<16; i++) {
-      if ($('.all__item').dataset.season === "1") {
-        this.hide;
-        console.log('click');
-      }
-    }
+  $('.nav__item--summer').on('click', function () {
+
+    $(".all__item[data-season='0']").map(function (index, element) {
+      $(this).hide();
+    }).get();
+    $(".all__item[data-season='1']").map(function (index, element) {
+      $(this).show();
+    }).get();
+    $('.nav__item').removeClass('nav__item--active');
+    $('.nav__item--summer').addClass('nav__item--active');
+
   });
 
-  $('.header__menu, .header__location').on('click', function() {
+  $('.nav__item--winter').on('click', function () {
+    $(".all__item[data-season='1']").map(function (index, element) {
+      $(this).hide();
+    }).get();
+    $(".all__item[data-season='0']").map(function (index, element) {
+      $(this).show();
+    }).get();
+    $('.nav__item').removeClass('nav__item--active');
+    $('.nav__item--winter').addClass('nav__item--active');
+  });
+
+  $('.nav__item--all').on('click', function () {
+    $(".all__item[data-season='1']").map(function (index, element) {
+      $(this).show();
+    }).get();
+    $(".all__item[data-season='0']").map(function (index, element) {
+      $(this).show();
+    }).get();
+    $('.nav__item').removeClass('nav__item--active');
+    $('.nav__item--all').addClass('nav__item--active');
+  });
+
+  $('.header__menu, .header__location').on('click', function () {
     $('.menu').toggleClass('menu--active');
   });
 
-  $('.content__btn, .item__btn').on('click', function() {
+  $('.content__btn, .item__btn').on('click', function () {
     $('.popup').addClass('popup--active');
     $('.bg').addClass('bg--active');
   });
 
-  $('.popup__exit, .bg').on('click', function() {
+  $('.popup__exit, .bg').on('click', function () {
     $('.popup').removeClass('popup--active');
     $('.bg').removeClass('bg--active');
   });
 
-  $('.slide__btn').on('click', function() {
+  $('.slide__btn').on('click', function () {
     $('.popup-reserve').addClass('popup-reserve--active');
     $('.bg').addClass('bg--active');
   });
 
-  $('.popup__exit, .bg').on('click', function() {
+  $('.popup__exit, .bg').on('click', function () {
     $('.popup-reserve').removeClass('popup-reserve--active');
     $('.bg').removeClass('bg--active');
   });
